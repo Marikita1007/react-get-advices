018af788bef92d5bf5fbc042a63d7b70fea639f5
Marika Abe <59960189+Marikita1007@users.noreply.github.com>
2024-05-27 12:52:43 +0000

    Called Advice API using useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  }
+
   return (
     <div>
-      <h1>Hellow World</h1>
-      <button>Get Advice</button>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
     </div>
   );
 }
